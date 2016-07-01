eafa493ed647547c263dc1353ed45e6b3f27a344
lusternite <luster_nite@hotmail.com>
2016-07-02 10:54:07 +1200

    Initial Commit

---
+++ Assets/EnemyBehaviour.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public enum EnemyState
+    {
+        Seeking,
+        Attacking,
+        Recovering,
+        Stunned
+    }
+
+    public Material[] EnemyColors;
+    public EnemyState CurrentEnemyState;
+    public GameObject ThePlayer;
+    public PlayerBehaviour PlayerScript;
+    public float EnemyVelocity = 1.0f;
+    Vector3 AttackPosition;
+    float AttackTimer;
+    public float RecoveryTimer;
+    public bool IsActive;
+    public float StunDuration;
+    public int Health = 2;
+
+    // Use this for initialization
+    void Start () {
+        CurrentEnemyState = EnemyState.Seeking;
+        ThePlayer = GameObject.FindGameObjectWithTag("Player");
+        PlayerScript = ThePlayer.GetComponent<PlayerBehaviour>();
+        IsActive = true;
+        AttackTimer = 9000.1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsActive)
+        {
+            if (PlayerScript.HasDrawn)
+            {
+                DoThings();
+            }
+        }
+	}
+
+    void OnCollisionEnter(Collision Col)
+    {
+        if (Col.gameObject.tag == "Player")
+        {
+            if (PlayerScript.CurrentPlayerState == PlayerBehaviour.PlayerState.Attacking && PlayerScript.AttackingTimer < AttackTimer)
+            {
+                PlayerScript.UpArrowFlag = false;
+                PlayerScript.DownArrowFlag = false;
+                PlayerScript.LeftArrowFlag = false;
+                PlayerScript.RightArrowFlag = false;
+                PlayerScript.GetComponent<Rigidbody>().velocity *= 0.0f;
+                PlayerScript.UpVelocity /= 5.0f;
+                PlayerScript.RightVelocity /= 5.0f;
+                PlayerScript.AttackingDuration = 0.0f;
+                PlayerScript.GetComponent<MeshRenderer>().material = PlayerScript.PlayerColors[3];
+                if (Health == 2)
+                {
+                    if (!PlayerScript.HasDrawn)
+                    {
+                        PlayerScript.HasDrawn = true;
+                        PlayerScript.IsAttacking = false;
+                        PlayerScript.CurrentPlayerState = PlayerBehaviour.PlayerState.Idle;
+                        PlayerScript.GetComponent<MeshRenderer>().material = PlayerScript.PlayerColors[0];
+                    }
+                    else
+                    {
+                        PlayerScript.RecoveryTimer = 0.5f;
+                        PlayerScript.CurrentPlayerState = PlayerBehaviour.PlayerState.Recovering;
+                    }
+                    StunDuration = 3.0f;
+                    Health -= 1;
+                    CurrentEnemyState = EnemyState.Stunned;
+                    GetComponent<MeshRenderer>().material = EnemyColors[2];
+                    AttackTimer = 9000.1f;
+                }
+                else
+                {
+                    PlayerScript.RecoveryTimer = 0.5f;
+                    PlayerScript.CurrentPlayerState = PlayerBehaviour.PlayerState.Recovering;
+                    Destroy(gameObject);
+                }
+            }
+            else if (PlayerScript.CurrentPlayerState == PlayerBehaviour.PlayerState.Parrying)
+            {
+                PlayerScript.UpArrowFlag = false;
+                PlayerScript.DownArrowFlag = false;
+                PlayerScript.LeftArrowFlag = false;
+                PlayerScript.RightArrowFlag = false;
+
+                StunDuration = 2.0f;
+                CurrentEnemyState = EnemyState.Stunned;
+                GetComponent<MeshRenderer>().material = EnemyColors[2];
+                PlayerScript.RecoveryTimer = 0.2f;
+                PlayerScript.CurrentPlayerState = PlayerBehaviour.PlayerState.Recovering;
+                AttackTimer = 9000.1f;
+            }
+            else
+            {
+                Destroy(Col.gameObject);
+                IsActive = false;
+            }
+            
+            Debug.Log("COLLISION");
+            Debug.Log("Player timer = " + PlayerScript.AttackingTimer);
+            Debug.Log("AI timer = " + AttackTimer);
+        }
+    }
+
+    void DoThings()
+    {
+        switch (CurrentEnemyState)
+        {
+            case EnemyState.Seeking:
+                {
+                    //Debug.Log(ThePlayer.GetComponent<Transform>().position);
+                    transform.position = Vector3.MoveTowards(transform.position, ThePlayer.GetComponent<Transform>().position, EnemyVelocity * Time.deltaTime);
+                    if (Vector3.Distance(transform.position, ThePlayer.GetComponent<Transform>().position) <= 2.5f)
+                    {
+                        CurrentEnemyState = EnemyState.Attacking;
+                        GetComponent<MeshRenderer>().material = EnemyColors[1];
+                        AttackPosition = ThePlayer.GetComponent<Transform>().position;
+                        AttackTimer = Time.time;
+                    }
+                    break;
+                }
+            case EnemyState.Attacking:
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, AttackPosition, EnemyVelocity * Time.deltaTime * 5.0f);
+                    if (Vector3.Distance(transform.position, AttackPosition) == 0.0f)
+                    {
+                        CurrentEnemyState = EnemyState.Recovering;
+                        GetComponent<MeshRenderer>().material = EnemyColors[2];
+                        RecoveryTimer = 2.0f;
+                        AttackTimer = 9000.1f;
+                    }
+                    break;
+                }
+            case EnemyState.Recovering:
+                {
+                    RecoveryTimer -= Time.deltaTime;
+                    if (RecoveryTimer <= 0.0f)
+                    {
+                        CurrentEnemyState = EnemyState.Seeking;
+                        GetComponent<MeshRenderer>().material = EnemyColors[0];
+                    }
+                    break;
+                }
+            case EnemyState.Stunned:
+                {
+                    StunDuration -= Time.deltaTime;
+                    if (StunDuration <= 0.0f)
+                    {
+                        CurrentEnemyState = EnemyState.Seeking;
+                        GetComponent<MeshRenderer>().material = EnemyColors[0];
+                    }
+                    break;
+                }
+        }
+    }
+}
---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,269 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public enum PlayerState
+    {
+        Idle,
+        Attacking,
+        Parrying,
+        Evading,
+        Recovering
+    }
+    public float MoveSpeed = 1.0f;
+    public bool UpArrowFlag = false;
+    public bool LeftArrowFlag = false;
+    public bool DownArrowFlag = false;
+    public bool RightArrowFlag = false;
+    public Vector3 UpVelocity;
+    public Vector3 RightVelocity;
+    public bool IsAttacking = false;
+    public float AttackingTimer = 9000.2f;
+    public float AttackingCooldown = 0.0f;
+    public float AttackingDuration = 0.0f;
+    public float ParryDuration = 0.0f;
+    public float EvadeDuration = 0.0f;
+    public float RecoveryTimer = 0.0f;
+    public bool Collided = false;
+    public bool HasDrawn = false;
+    public PlayerState CurrentPlayerState = PlayerState.Idle;
+    public Material[] PlayerColors;
+    public bool IsParryStance = true;
+    public bool IaiParry = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        UpVelocity = new Vector3(0.0f, 0.0f, 1.0f);
+        RightVelocity = new Vector3(1.0f, 0.0f, 0.0f);
+        RecoveryTimer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleDirectionalInput();
+        HandleSlashing();
+        HandleDodge();
+        HandleParrying();
+        HandleRecovery();
+        ChangeStance();
+        TextToggle();
+    }
+
+    void HandleDirectionalInput()
+    {
+        if (CurrentPlayerState != PlayerState.Recovering && CurrentPlayerState != PlayerState.Parrying)
+        {
+            if (Input.GetKeyDown("up"))
+            {
+                UpArrowFlag = true;
+                GetComponent<Rigidbody>().velocity += UpVelocity;
+            }
+            if (Input.GetKeyDown("left"))
+            {
+                LeftArrowFlag = true;
+                GetComponent<Rigidbody>().velocity -= RightVelocity;
+            }
+            if (Input.GetKeyDown("down"))
+            {
+                DownArrowFlag = true;
+                GetComponent<Rigidbody>().velocity -= UpVelocity;
+            }
+            if (Input.GetKeyDown("right"))
+            {
+                RightArrowFlag = true;
+                GetComponent<Rigidbody>().velocity += RightVelocity;
+            }
+
+            if (Input.GetKeyUp("right"))
+            {
+                if (RightArrowFlag)
+                {
+                    RightArrowFlag = false;
+                    GetComponent<Rigidbody>().velocity -= RightVelocity;
+                }
+            }
+            if (Input.GetKeyUp("up"))
+            {
+                if (UpArrowFlag)
+                {
+                    UpArrowFlag = false;
+                    GetComponent<Rigidbody>().velocity -= UpVelocity;
+                }
+            }
+            if (Input.GetKeyUp("down"))
+            {
+                if (DownArrowFlag)
+                {
+                    DownArrowFlag = false;
+                    GetComponent<Rigidbody>().velocity += UpVelocity;
+                }
+            }
+            if (Input.GetKeyUp("left"))
+            {
+                if (LeftArrowFlag)
+                {
+                    LeftArrowFlag = false;
+                    GetComponent<Rigidbody>().velocity += RightVelocity;
+                }
+            }
+        }
+    }
+
+    void HandleRecovery()
+    {
+        if (RecoveryTimer > 0.0f)
+        {
+            RecoveryTimer -= Time.deltaTime;
+            if (RecoveryTimer < 0.0f)
+            {
+                RecoveryTimer = 0.0f;
+                CurrentPlayerState = PlayerState.Idle;
+                GetComponent<MeshRenderer>().material = PlayerColors[0];
+            }
+        }
+    }
+
+    void HandleSlashing()
+    {
+        if (Input.GetKeyDown("space") && AttackingDuration == 0.0f && CurrentPlayerState == PlayerState.Idle)
+        {
+            CurrentPlayerState = PlayerState.Attacking;
+            IsAttacking = true;
+            AttackingTimer = Time.time;
+            if (IsParryStance)
+            {
+                AttackingDuration = 0.2f;
+            }
+            else
+            {
+                AttackingDuration = 0.5f;
+            }
+
+            //AttackingCooldown = 2.0f;
+            GetComponent<Rigidbody>().velocity *= 5.0f;
+            UpVelocity *= 5.0f;
+            RightVelocity *= 5.0f;
+            GetComponent<MeshRenderer>().material = PlayerColors[1];
+        }
+        if (AttackingDuration > 0.0f)
+        {
+            AttackingDuration -= Time.deltaTime;
+            if (AttackingDuration < 0.0f)
+            {
+                CurrentPlayerState = PlayerState.Recovering;
+                AttackingDuration = 0.0f;
+                IsAttacking = false;
+                if (IsParryStance)
+                {
+                    RecoveryTimer = 0.3f;
+                }
+                else
+                {
+                    RecoveryTimer = 0.5f;
+                }
+                GetComponent<Rigidbody>().velocity *= 0.0f;
+                UpVelocity /= 5.0f;
+                RightVelocity /= 5.0f;
+                AttackingTimer = 9000.2f;
+                UpArrowFlag = false;
+                DownArrowFlag = false;
+                LeftArrowFlag = false;
+                RightArrowFlag = false;
+                GetComponent<MeshRenderer>().material = PlayerColors[3];
+            }
+        }
+    }
+
+    void HandleParrying()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && ParryDuration == 0.0f && CurrentPlayerState == PlayerState.Idle && IsParryStance)
+        {
+            CurrentPlayerState = PlayerState.Parrying;
+            GetComponent<Rigidbody>().velocity *= 0.0f;
+            ParryDuration = 0.3f;
+            GetComponent<MeshRenderer>().material = PlayerColors[2];
+            if (!HasDrawn)
+            {
+                HasDrawn = true;
+                IaiParry = true;
+            }
+        }
+        if (ParryDuration > 0.0f)
+        {
+            ParryDuration -= Time.deltaTime;
+            if (ParryDuration < 0.0f)
+            {
+                ParryDuration = 0.0f;
+                if (IaiParry)
+                {
+                    IaiParry = false;
+                    CurrentPlayerState = PlayerState.Idle;
+                    GetComponent<MeshRenderer>().material = PlayerColors[0];
+                }
+                else
+                {
+                    RecoveryTimer = 0.2f;
+                    CurrentPlayerState = PlayerState.Recovering;
+                    GetComponent<MeshRenderer>().material = PlayerColors[3];
+                }
+                UpArrowFlag = false;
+                DownArrowFlag = false;
+                LeftArrowFlag = false;
+                RightArrowFlag = false;
+            }
+        }
+    }
+
+    void HandleDodge()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && CurrentPlayerState == PlayerState.Idle)
+        {
+            CurrentPlayerState = PlayerState.Evading;
+            GetComponent<Rigidbody>().velocity *= 8.0f;
+            UpVelocity *= 10.0f;
+            RightVelocity *= 10.0f;
+            EvadeDuration = 0.2f;
+        }
+        if (EvadeDuration > 0.0f)
+        {
+            EvadeDuration -= Time.deltaTime;
+            if (EvadeDuration < 0.0f)
+            {
+                CurrentPlayerState = PlayerState.Recovering;
+                RecoveryTimer = 0.1f;
+                EvadeDuration = 0.0f;
+                GetComponent<Rigidbody>().velocity *= 0.0f;
+                UpVelocity /= 10.0f;
+                RightVelocity /= 10.0f;
+                UpArrowFlag = false;
+                DownArrowFlag = false;
+                LeftArrowFlag = false;
+                RightArrowFlag = false;
+                GetComponent<MeshRenderer>().material = PlayerColors[3];
+            }
+        }
+    }
+
+    void ChangeStance()
+    {
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            IsParryStance = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            IsParryStance = false;
+        }
+    }
+
+    void TextToggle()
+    {
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            FindObjectOfType<Canvas>().enabled = !FindObjectOfType<Canvas>().enabled;
+        }
+    }
+}
